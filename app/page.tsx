644f8db5d175d69447cb3644b227e98415e009cf
Kim MyungSub <audtjqxx@naver.com>
2024-01-06 11:12:50 +0900

    [setting] 프로젝트 초기 세팅

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <button onClick={() => router.push("/login")}>로그인/회원가입</button>
+    </div>
+  );
+}
